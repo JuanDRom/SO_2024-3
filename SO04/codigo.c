29771f1de2e7706a791e032eb142d1ecf7d62ac7
JuanDRom <88343141+JuanDRom@users.noreply.github.com>
2024-09-24 20:46:43 -0500

    Update codigo.c

--- SO04/codigo.c
+++ SO04/codigo.c
@@ -1,12 +1,9 @@
 /*******************************************************************************************************
-Autor: Jocelyne Gonzalez Hernanadez
+Autor: Juan Diego Romero
 Fecha:03/09/2024
 Materia: Sistemas Operativos
 Tema:Introducción a procesos con Fork
-Descripción:
-*Fork: al ser invocado crea un proceso "hijo" y uno "padre".
-**Cada uno de estos al ser invocados tienen diferentes espacios de memoria (2 procesos diferentes).
-**Son idénticos hasta que el programador determine lo contrario.
+
 *******************************************************************************************************/
 
 #include <stdio.h>
